49e209fd877724f6af82675dd6899166ef183e04
Sofiavega12 <sofiavegachavarria@gmail.com>
2023-08-22 00:13:25 -0600

    2 pantallas
    Lobby estudiantes administrativo y comunicación

---
+++ app/src/main/java/com/example/eventec/LobbyEstudiantesAdmin.java
@@ -0,0 +1,14 @@
+package com.example.eventec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LobbyEstudiantesAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby_estudiantes_admin);
+    }
+}
