8b2034e31eff3728206628fd7aa9eb86b7aebf58
Alexbg08 <machobg08@estudiantec.cr>
2023-09-14 12:22:11 -0600

    update

--- app/src/main/java/com/example/eventec/MainActivity.java
+++ app/src/main/java/com/example/eventec/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, ConsultarActividades.class);
+        Intent intent = new Intent(this, AsignarColaborador.class);
         startActivity(intent);
         finish();
     }
