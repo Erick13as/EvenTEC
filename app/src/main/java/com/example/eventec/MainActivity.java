e8ac7702a3ff3c45ae94ed8ac91258ebf8624fb2
tefadc94 <tefadc@estudiantec.cr>
2023-09-14 15:53:06 -0600

    update creacion evento horas

--- app/src/main/java/com/example/eventec/MainActivity.java
+++ app/src/main/java/com/example/eventec/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, LobbyEstudiantesAdmin.class);
+        Intent intent = new Intent(this, CalendarioEventos.class);
         startActivity(intent);
         finish();
     }
