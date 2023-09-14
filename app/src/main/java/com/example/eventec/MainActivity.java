94ac57d353b979639ea6e62d1e42f57f59239341
tefadc94 <tefadc@estudiantec.cr>
2023-09-14 00:44:03 -0600

    registrar asociacion

--- app/src/main/java/com/example/eventec/MainActivity.java
+++ app/src/main/java/com/example/eventec/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, LobbyColaboradores.class);
+        Intent intent = new Intent(this, LobbyAsociaciones.class);
         startActivity(intent);
         finish();
     }
