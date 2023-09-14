212475dc1fe78db15feeacc57780ad963b4506bc
tefadc94 <tefadc@estudiantec.cr>
2023-09-14 16:30:52 -0600

    Update LobbyEstudiantes.java

--- app/src/main/java/com/example/eventec/LobbyEstudiantes.java
+++ app/src/main/java/com/example/eventec/LobbyEstudiantes.java
@@ -1,6 +1,9 @@
 package com.example.eventec;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,39 @@ public class LobbyEstudiantes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby_estudiantes);
+
+        Button btn_InscripcionEventos = findViewById(R.id.btn_InscripcionEventos);
+        btn_InscripcionEventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LobbyEstudiantes.this, Inscripcion.class);
+                startActivity(intent);
+
+                // Cierra la actividad actual (opcional, si deseas volver atrás)
+                finish();
+            }
+        });
+        Button btn_CalendarioDeEventos2 = findViewById(R.id.btn_CalendarioDeEventos2);
+        btn_CalendarioDeEventos2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LobbyEstudiantes.this, CalendarioEventos.class);
+                startActivity(intent);
+
+                // Cierra la actividad actual (opcional, si deseas volver atrás)
+                finish();
+            }
+        });
+        Button btn_MisEventos = findViewById(R.id.btn_MisEventos);
+        btn_MisEventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LobbyEstudiantes.this, MisEventos.class);
+                startActivity(intent);
+
+                // Cierra la actividad actual (opcional, si deseas volver atrás)
+                finish();
+            }
+        });
     }
 }
