a75154a66aac1bbd0f31717deee977e5397d1bde
Erick13as <alvaradosolanoerick@gmail.com>
2023-09-14 23:37:16 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/eventec/MainActivity.java
+++ app/src/main/java/com/example/eventec/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Login.class);
 
 
-
-
+        
         startActivity(intent);
         finish();
     }
